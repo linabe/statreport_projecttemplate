6599125c203fe555c54d7a7c8d5859f515decf16
Lina Benson <lina.benson@ki.se>
2019-06-25 15:53:15 +0200

    added defaultKableTable (with default options for knitr::kable, https://www.rdocumentation.org/packages/knitr, and kableExtra,  https://cran.r-project.org/web/packages/kableExtra/)

---
+++ lib/defaultKableTable.R
@@ -0,0 +1,21 @@
+mykable <- function(tab, caption = NULL) {
+  knitr::kable(tab,
+    booktabs = TRUE,
+    linesep = "",
+    row.names = FALSE,
+    caption = caption,
+    align = c(rep("l", ncol(tab)))
+  ) %>%
+    kable_styling(latex_options = c("striped", "hold_position", "repeat_header"), full_width = F)
+}
+
+## HTML option not used
+# mykable <- function(tab) {
+#   knitr::kable(tab,
+#     booktabs = TRUE,
+#     linesep = "",
+#     row.names = FALSE,
+#     align = c(rep("l", ncol(tab)))
+#   ) %>%
+#     kable_styling("striped", "hold_position", position = "left", full_width = F)
+# }
