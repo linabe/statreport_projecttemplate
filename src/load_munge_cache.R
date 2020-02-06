ebc38a1da7251acbc45229dddfa28f1142f77cf7
Lina Benson <lina.benson@ki.se>
2020-02-06 08:08:09 +0100

    workflow with munge and cache so don't run munge every time

---
+++ src/load_munge_cache.R
@@ -0,0 +1,17 @@
+# 1. Set options in config/global.dcf
+# 2. Load packages listed in config/global.dcf
+# 3. Import functions and code in lib directory
+# 4. Load data in data directory
+# 5. Run data manipulations in munge directory
+
+ProjectTemplate::reload.project(
+  reset = TRUE,
+  data_loading = TRUE,
+  munging = TRUE
+)
+
+#ProjectTemplate::cache("flow")
+#ProjectTemplate::cache("wdata")
+#ProjectTemplate::cache("imp")
+#ProjectTemplate::cache("imp.org")
+#ProjectTemplate::cache("noimpvars")
